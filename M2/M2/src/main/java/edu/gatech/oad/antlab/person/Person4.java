69f1650ec3309eed06328976d8f21c120efeca0c
Karl Nicodemus <k.nicodemus@gatech.edu>
2015-09-01 19:50:21 -0400

    Person4 finished
    Finished my part for Person4

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,8 +5,8 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
- *  @version 1.1
+ *  @author Karl Nicodemus
+ *  @version 1.2
  */
 public class Person4 {
   /** Holds the persons real name */
@@ -31,7 +31,13 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+        String retString = "";
+        for(int x = 0; x < input.length(); x++){
+            int temp = (int) input.charAt(x);
+            temp = temp + 1;
+            retString = retString + (char) temp;
+        }
+      return retString;
     }
     
     /**
