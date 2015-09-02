523deb3c4c936f1c5ed73ca11eae37ff61821130
suwilliam <williamsu@gatech.edu>
2015-09-01 20:28:35 -0400

    edited AntLabMain.java

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -66,7 +66,7 @@ import edu.gatech.oad.antlab.pkg2.*;
   		  //Person3 replace P3 with your name
   		  //and gburdell3 with your gt id
   		  Person3 p3 = new Person3("William Su");
-  		  toPrint += p3.toString("gburdell3");
+  		  toPrint += p3.toString("wwilliam6");
         //Person4 replace P4 with your name
         //and gburdell4 with your gt id
         Person4 p4 = new Person4("Karl Nicodemus");
