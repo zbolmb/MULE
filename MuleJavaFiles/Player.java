f68d275f30968d260e247ee1ed215b253fe3d49b
William Hsu <william7hsu@gmail.com>
2015-09-10 20:50:48 -0400

    test

---
+++ MuleJavaFiles/Player.java
@@ -0,0 +1,13 @@
+import javafx.scene.paint.Color;
+
+/**
+ * Created by William on 9/10/2015.
+ * Race: 0 = Elin, 1 = Blood Elves, 2 = Orc, 3 = High Human, 4 = Protoss
+ */
+public class Player {
+	protected String name;
+	protected int race;
+	protected Color color;
+
+
+}
