0e98f3b7d5bb7ffab54c21ce5073b262307fcb84
William Hsu <william7hsu@gmail.com>
2015-09-28 19:44:16 -0400

    town turn

---
+++ MuleJavaFiles/TownTurn.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+/**
+ * Created by willi on 9/28/2015.
+ */
+public class TownTurn extends TurnTracker {
+    protected ArrayList<Player> players;
+    protected int round;
+
+    public TownTurn(ArrayList<Player> players) {
+        super(players);
+    }
+
+    private Player getRichGuy() {
+        if (players.size() == 0) {
+            return null;
+        }
+        Player moneyMan = players.get(0);
+        for (int i = 0; i < players.size(); i++) {
+            Player currPlayer = players.get(i);
+            if (currPlayer.money > moneyMan.money) {
+                moneyMan = currPlayer;
+            }
+        }
+        players.remove(moneyMan);
+        return moneyMan;
+    }
+
+    
+}
