6ff1cf48ddb68c0d921105a128e062d0a8d51b03
William Hsu <william7hsu@gmail.com>
2015-10-01 00:46:21 -0400

    fixed the town

--- MuleJavaFiles/TownMap.java
+++ MuleJavaFiles/TownMap.java
@@ -8,7 +8,7 @@ public class TownMap {
     protected TownTiles[][] aMap;
 
     public TownMap() {
-        this.aMap = new TownTiles[3][5];
+        this.aMap = new TownTiles[5][3];
     }
 
     /**
@@ -17,25 +17,22 @@ public class TownMap {
      */
     public void createDefaultGameMap() {
         aMap[0][0] = new crystiteTownTile();
-        aMap[1][0] = new floorTile();
-        aMap[2][0] = new assayTownTile();
+        aMap[1][0] = new smithoreTownTile();
+        aMap[2][0] = new floorTile();
+        aMap[3][0] = new energyTownTile();
+        aMap[4][0] = new foodTownTile();
 
-        aMap[0][1] = new smithoreTownTile();
+        aMap[0][1] = new floorTile();
         aMap[1][1] = new floorTile();
-        aMap[2][1] = new landTownTile();
+        aMap[2][1] = new floorTile();
+        aMap[3][1] = new floorTile();
+        aMap[4][1] = new floorTile();
 
-
-        aMap[0][2] = new floorTile();
-        aMap[1][2] = new floorTile();
+        aMap[0][2] = new assayTownTile();
+        aMap[1][2] = new landTownTile();
         aMap[2][2] = new floorTile();
-
-        aMap[0][3] = new energyTownTile();
-        aMap[1][3] = new floorTile();
-        aMap[2][3] = new pubTile();
-
-        aMap[0][4] = new foodTownTile();
-        aMap[1][4] = new floorTile();
-        aMap[2][4] = new muleTile();
+        aMap[3][2] = new pubTile();
+        aMap[4][2] = new muleTile();
 
 
     }
