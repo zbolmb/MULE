f39c44c71ddcf4225e9f18f03546f3b199bc240c
Zhijian <zhijianli@gatech.edu>
2015-10-21 12:05:36 -0400

    Removed extra directory

--- MuleJavaFiles/landTiles/m2Tile.java
+++
@@ -1,9 +0,0 @@
-package landTiles;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-
-public class m2Tile extends MapTiles {
-	public m2Tile() {
-	    super(1, 1, 3, true, true, "Mountain 2", Color.BROWN, new Image("Tiles/m2_Tile.jpg"));
-	}
-}
--- MuleJavaFiles/landTiles/plainsTile.java
+++
@@ -1,9 +0,0 @@
-package landTiles;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-
-public class plainsTile extends MapTiles {
-	public plainsTile() {
-	    super(2, 3, 1, true, false, "Plains", Color.YELLOW, new Image("Tiles/plains_Tile.jpg"));
-	}
-}
