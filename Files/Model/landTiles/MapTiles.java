ada4254824a5bf62eb78457ed5de486b65833035
William Hsu <william7hsu@gmail.com>
2015-11-04 23:24:48 -0500

    rips checkstyle stuff

--- Files/Model/landTiles/MapTiles.java
+++ Files/Model/landTiles/MapTiles.java
@@ -14,8 +14,8 @@ import javafx.scene.shape.Rectangle;
 
 public abstract class MapTiles {
 
-    String owner;
-    boolean[] mules;
+    private String owner;
+    private boolean[] mules;
 
     private int food;
     private int energy;
@@ -33,15 +33,24 @@ public abstract class MapTiles {
     private Color mapType;
     //-----------------------------------------------------------------
 
+
+
+
     /**
-     * @param food
-     * @param energy
-     * @param ore
-     * @param crystite
-     * @param highlightable
+     * The map tile class
+     * @param food the food generated
+     * @param energy the energy generated
+     * @param ore the ore generated
+     * @param crystite whether crystite is present
+     * @param highlightable if it is highlitable
      * @param mapType ** This is temporary. Should be an image **
+     * @param name The name of the tile
+     * @param img The image of the tile
+     *
      */
-    public MapTiles(int food, int energy, int ore, boolean crystite, boolean highlightable, String name, Color mapType, Image img) {
+    public MapTiles(int food, int energy, int ore, boolean crystite,
+                    boolean highlightable, String name,
+                    Color mapType, Image img) {
         this.food = food;
         this.energy = energy;
         this.ore = ore;
@@ -54,35 +63,204 @@ public abstract class MapTiles {
         this.img = img;
         this.mules = new boolean[3];
     }
+    /**
+    * Getters and Setters
+    * @return various
+    */
+    public int getFood() {
+        return food;
+    }
 
     /**
-     * Getters and Setters
-     * @return various
+     * SetFood
+     * @param food set the food
      */
+    public void setFood(int food) {
+        this.food = food;
+    }
 
-    public int getFood() { return food; }
-    public void setFood(int food) { this.food = food; }
-    public int getEnergy() { return energy; }
-    public void setEnergy(int energy) { this.energy = energy; }
-    public int getOre() { return ore; }
-    public void setOre(int ore) { this.ore = ore; }
-    public boolean isCrystalite() { return crystite; }
-    public void setCrystalite(boolean crystite) { this.crystite = crystite; }
-    public boolean isHighlightable() { return highlightable; }
-    public void setHighlightable(boolean highlightable) { this.highlightable = highlightable; }
-    public Color getMapType() { return mapType; }
-    public void getMapType(Color mapType) { this.mapType = mapType; }
-    public String getName() { return name; }
-    public static int getW() { return w; };
-    public static int getH() { return h; };
-    public Rectangle getMapTileGui() { return mapTileGui;}
-    public void setMapTileGui(Rectangle mapTileGui) { this.mapTileGui = mapTileGui; };
-    public String getOwner() { return owner; }
-    public void setOwner(String owner) { this.owner = owner; }
-    public Image getImg() { return img; }
-    public void setImg(Image img) { this.img = img; }
-    public ImageView getImgView() { return new ImageView(img); }
-    public boolean getMule(int m) { return mules[m]; }
-    public void setMule(int m) { mules[m] = true; }
-    public boolean[] getMules() { return mules; }
+    /**
+     * get energy
+     * @return the energy
+     */
+    public int getEnergy() {
+        return energy;
+    }
+
+    /**
+     * set energy
+     * @param energy takes in energy
+     */
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    /**
+     * get the ore
+     * @return the ore
+     */
+    public int getOre() {
+        return ore;
+    }
+
+    /**
+     * set ore
+     * @param ore takes in the ore
+     */
+    public void setOre(int ore) {
+        this.ore = ore;
+    }
+
+    /**
+     * checks if crystalite
+     * @return the crystite
+     */
+    public boolean isCrystalite() {
+        return crystite;
+    }
+
+    /**
+     * set crystite
+     * @param crystite takes in crystite
+     */
+    public void setCrystalite(boolean crystite) {
+        this.crystite = crystite;
+    }
+
+    /**
+     * checks if highlightable
+     * @return return boolean
+     */
+    public boolean isHighlightable() {
+        return highlightable;
+    }
+
+    /**
+     * sethighlightable
+     * @param highlightable get whether it is highlightable
+     */
+    public void setHighlightable(boolean highlightable) {
+        this.highlightable = highlightable;
+    }
+
+    /**
+     * getmaptype method
+     * @return maptype
+     */
+    public Color getMapType() {
+        return mapType;
+    }
+
+    /**
+     * get maptype
+     * @param mapType takes in maptype
+     */
+    public void getMapType(Color mapType) {
+        this.mapType = mapType;
+    }
+
+    /**
+     * getName
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * getW
+     * @return the w
+     */
+    public static int getW() {
+        return w;
+    };
+
+    /**
+     * getH
+     * @return the h
+     */
+    public static int getH() {
+        return h;
+    };
+
+    /**
+     * getMapTileGui
+     * @return maptileGui
+     */
+    public Rectangle getMapTileGui() {
+        return mapTileGui;
+    }
+
+    /**
+     * setMapTileGui
+     * @param mapTileGui mapTileGui
+     */
+    public void setMapTileGui(Rectangle mapTileGui) {
+        this.mapTileGui = mapTileGui;
+    };
+
+    /**
+     * getOwner function
+     * @return owner
+     */
+    public String getOwner() {
+        return owner;
+    }
+
+    /**
+     * setOwner function
+     * @param owner the owner param
+     */
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    /**
+     * getImg method
+     * @return img img
+     */
+    public Image getImg() {
+        return img;
+    }
+
+    /**
+     * setImg
+     * @param img takes in img
+     */
+    public void setImg(Image img) {
+        this.img = img;
+    }
+
+    /**
+     * getimgview
+     * @return img
+     */
+    public ImageView getImgView() {
+        return new ImageView(img);
+    }
+
+    /**
+     * getMule method
+     * @param m m
+     * @return mule
+     */
+    public boolean getMule(int m) {
+        return mules[m];
+    }
+
+    /**
+     * setMule method
+     * @param m m
+     */
+    public void setMule(int m) {
+        mules[m] = true;
+    }
+
+    /**
+     * getMuels
+     * @return mules
+     */
+    public boolean[] getMules() {
+        return mules;
+    }
 }
--- Files/Model/landTiles/m3Tile.java
+++ Files/Model/landTiles/m3Tile.java
@@ -4,7 +4,10 @@ import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
 public class m3Tile extends MapTiles{
-	public m3Tile() {
-		super(1, 1, 4, true, true, "Mountain 3", Color.web("0x382E1C"), new Image("Model/Tiles/m3_Tile.jpg"));
-	}
+    /**
+     * The m3Tile class
+     */
+    public m3Tile() {
+        super(1, 1, 4, true, true, "Mountain 3", Color.web("0x382E1C"), new Image("Model/Tiles/m3_Tile.jpg"));
+    }
 }
