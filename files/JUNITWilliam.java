a674760548685445995567068232510d92ea962b
William Hsu <william7hsu@gmail.com>
2015-11-12 23:40:45 -0500

    Junit

--- files/JUNITWilliam.java
+++ files/JUNITWilliam.java
@@ -1,21 +1,25 @@
 package files;
 import files.model.Configurations;
 import files.model.Player;
-import files.model.landTiles.*;
+import files.controller.Util;
+import files.model.landTiles.M1Tile;
+import files.model.landTiles.M2Tile;
+import files.model.landTiles.M3Tile;
+import files.model.landTiles.MapTiles;
+import files.model.landTiles.PlainsTile;
+import files.model.landTiles.RiverTile;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertArrayEquals;
 
 /**
  * Created by William on 11/12/2015.
  */
-public class JUNITWilliam {
+public class JunitWilliam {
     public static final int TIMEOUT = 200;
     private ArrayList<Player> players;
     private ArrayList<MapTiles> tiles1;
@@ -27,19 +31,52 @@ public class JUNITWilliam {
     public void setUp() {
         players = new ArrayList<>();
         tiles1 = new ArrayList<>();
-        tiles1.add((MapTiles) new PlainsTile(1));
+        MapTiles t1 = new PlainsTile(1);
+        t1.setMule(0);
+        tiles1.add(t1);
         tiles2 = new ArrayList<>();
-        tiles2.add((MapTiles) new RiverTile(0));
+        MapTiles t2 = new RiverTile(0);
+        t2.setMule(0);
+        t2.setMule(1);
+        t2.setMule(2);
+        tiles2.add(t2);
         tiles3 = new ArrayList<>();
-        tiles3.add((MapTiles) new M1Tile(1));
+        MapTiles t3 = new M1Tile(1);
+        tiles3.add(t3);
+        t3.setMule(1);
         tiles4 = new ArrayList<>();
-        tiles4.add((MapTiles) new M2Tile(1));
-        tiles4.add((MapTiles) new M3Tile(1));
+        MapTiles t4 = new M2Tile(1);
+        t4.setMule(2);
+        MapTiles t5 = new M3Tile(1);
+        t5.setMule(2);
+        tiles4.add(t4);
+        tiles4.add(t5);
     }
 
     @Test(timeout = TIMEOUT)
-    public void testProduce() {
+    public void testProduce() throws IOException {
         Player p1 = new Player();
         p1.setOwned(tiles1);
+        Player p2 = new Player();
+        p2.setOwned(tiles2);
+        Player p3 = new Player();
+        p3.setOwned(tiles3);
+        Player p4 = new Player();
+        p4.setOwned(tiles4);
+        players.add(p1);
+        players.add(p2);
+        players.add(p3);
+        players.add(p4);
+        Configurations.setPlayers(players);
+        Util.produce();
+        assertEquals(2, players.get(0).getFood());
+        assertEquals(4, players.get(1).getFood());
+        assertEquals(3, players.get(1).getEnergy());
+        assertEquals(0, players.get(1).getSmithore());
+        assertEquals(4, players.get(2).getEnergy());
+        assertEquals(7, players.get(3).getSmithore());
+
+
+
     }
 }
